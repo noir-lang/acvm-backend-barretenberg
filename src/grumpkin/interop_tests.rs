8f0148dd638cf1e9ea390f13d85bced8d68b13b5
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2022-08-12 17:34:22 +0100

    add code to deserialise a point

--- src/grumpkin/interop_tests.rs
+++ src/grumpkin/interop_tests.rs
@@ -1,5 +1,5 @@
-use ark_ec::{msm::VariableBaseMSM as ArkVariableBaseMSM, AffineCurve};
-use ark_ff::{bytes::FromBytes, field_new, BigInteger, PrimeField, Zero};
+use ark_ec::{AffineCurve, ProjectiveCurve};
+use ark_ff::{BigInteger256, PrimeField, Zero};
 use ark_serialize::CanonicalSerialize;
 use grumpkin::{Fq, SWAffine, SWProjective};
 
@@ -24,4 +24,38 @@ fn c_plus_plus_interop_generator() {
 
     assert_eq!(hex::encode(bytes_x), expected_x);
     assert_eq!(hex::encode(bytes_y), expected_y);
+}
+fn deserialise_fq(bytes: &[u8]) -> Option<Fq> {
+    assert_eq!(bytes.len(), 32);
+
+    let mut tmp = BigInteger256([0, 0, 0, 0]);
+
+    tmp.0[3] = u64::from_be_bytes(<[u8; 8]>::try_from(&bytes[0..8]).unwrap());
+    tmp.0[2] = u64::from_be_bytes(<[u8; 8]>::try_from(&bytes[8..16]).unwrap());
+    tmp.0[1] = u64::from_be_bytes(<[u8; 8]>::try_from(&bytes[16..24]).unwrap());
+    tmp.0[0] = u64::from_be_bytes(<[u8; 8]>::try_from(&bytes[24..32]).unwrap());
+
+    Fq::from_repr(tmp)
+}
+
+fn deserialise_point(x_bytes: &[u8], y_bytes: &[u8]) -> Option<SWAffine> {
+    let x = deserialise_fq(x_bytes)?;
+    let y = deserialise_fq(y_bytes)?;
+    let is_infinity = false; // none of the generators should be points at infinity
+    Some(SWAffine::new(x, y, is_infinity))
+}
+
+#[test]
+fn c_plus_plus_interop_generator_deserialise() {
+    // Copied from the C++ codebase
+    let expected_x = "0000000000000000000000000000000000000000000000000000000000000001";
+    let expected_y = "0000000000000002cf135e7506a45d632d270d45f1181294833fc48d823f272c";
+
+    let x_bytes = hex::decode(expected_x).unwrap();
+    let y_bytes = hex::decode(expected_y).unwrap();
+    let got_point = deserialise_point(&x_bytes, &y_bytes).unwrap();
+
+    let expected_point = SWAffine::prime_subgroup_generator();
+
+    assert_eq!(got_point, expected_point)
 }
