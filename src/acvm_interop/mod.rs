f5d8cdbc3eae306bc39d67b344b75198ed720db5
kevaundray <kevtheappdev@gmail.com>
2022-08-18 21:39:14 +0000

    since proof system and smart contract are only available via FFI, we put those behind the sys feature

--- src/acvm_interop/mod.rs
+++ src/acvm_interop/mod.rs
@@ -1,8 +1,9 @@
-use acvm::Backend;
-
+#[cfg(feature = "sys")]
 pub mod proof_system;
 pub mod pwg;
+#[cfg(feature = "sys")]
 mod smart_contract;
 pub struct Plonk;
 
-impl Backend for Plonk {}
+#[cfg(feature = "sys")]
+impl acvm::Backend for Plonk {}
