6f7f9339bac8b9df8439da99278334b18b2acbb9
kevaundray <kevtheappdev@gmail.com>
2022-08-19 14:16:16 +0000

    expose a method to compute partial witnesses in wasm

--- src/lib.rs
+++ src/lib.rs
@@ -13,3 +13,58 @@ pub use serialiser::serialise_circuit;
 pub mod acvm_interop;
 pub use acvm_interop::Plonk;
 mod barretenberg_structures;
+
+#[cfg(feature = "wasm")]
+use wasm_bindgen::prelude::*;
+
+#[cfg(feature = "wasm")]
+use acvm::{
+    acir::circuit::Circuit, acir::native_types::Witness, FieldElement, PartialWitnessGenerator,
+};
+#[cfg(feature = "wasm")]
+use std::collections::BTreeMap;
+
+// Flattened
+pub type ComputedWitness = Vec<u8>;
+// TODO: currently we only use u32 as witness values, this should be changed to
+// TODO: bytes as field elements are generally 32 bytes or more
+#[cfg(feature = "wasm")]
+#[wasm_bindgen]
+pub fn compute_witnesses(circuit: JsValue, initial_witness: Vec<u32>) -> ComputedWitness {
+    let circuit: Circuit = circuit.into_serde().unwrap();
+
+    // Convert initial witness vector to a BTreeMap and add the zero witness as the first one
+    let mut witness_map: BTreeMap<Witness, FieldElement> = BTreeMap::new();
+    // witness_map.insert(Witness(0), FieldElement::zero());
+    let num_wits = circuit.current_witness_index;
+    for (index, element) in initial_witness.into_iter().enumerate() {
+        witness_map.insert(
+            Witness((index + 1) as u32),
+            FieldElement::from(element as u128),
+        );
+    }
+    debug_assert_eq!((num_wits + 1) as usize, witness_map.len());
+
+    // Now use the partial witness generator to fill in the rest of the witnesses
+    // which are possible
+
+    let plonk = Plonk;
+    match plonk.solve(&mut witness_map, circuit.gates) {
+        Ok(_) => {}
+        Err(opcode) => panic!("solver came across an error with opcode {}", opcode),
+    };
+
+    // let field_values_as_bytes: Vec<_> =
+    //     witness_map.into_iter().map(|(_, field_val)| field_val.to_bytes()).flatten().collect();
+    // field_values_as_bytes
+
+    // Serialise the witness in a way that the C++ codebase can deserialise
+    let assignments = crate::barretenberg_structures::Assignments::from_vec(
+        witness_map
+            .into_iter()
+            .map(|(_, field_val)| field_val)
+            .collect(),
+    );
+
+    assignments.to_bytes()
+}
