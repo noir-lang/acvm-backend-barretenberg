6761f70ab1fee444d0aa902b44d4c47a749f5e1c
kevaundray <kevtheappdev@gmail.com>
2022-09-13 15:41:10 +0000

    disable proof system adn smart contract for `wasm` generation feature

--- src/acvm_interop/mod.rs
+++ src/acvm_interop/mod.rs
@@ -1,7 +1,8 @@
-
+#[cfg(not(feature = "wasm"))]
 pub mod proof_system;
 pub mod pwg;
+#[cfg(not(feature = "wasm"))]
 mod smart_contract;
 pub struct Plonk;
-
+#[cfg(not(feature = "wasm"))]
 impl acvm::Backend for Plonk {}
