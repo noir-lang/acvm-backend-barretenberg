bbfd409a541b96cd8ce0a867b279b7da6c5b94fc
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2022-10-31 20:41:30 +0000

    remove old src directory

--- src/acvm_interop/mod.rs
+++
@@ -1,8 +0,0 @@
-#[cfg(not(feature = "wasm"))]
-pub mod proof_system;
-pub mod pwg;
-#[cfg(not(feature = "wasm"))]
-mod smart_contract;
-pub struct Plonk;
-#[cfg(not(feature = "wasm"))]
-impl acvm::Backend for Plonk {}
