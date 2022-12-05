fc319bdb33edbf6f5121413f0f75a562b92b0828
kevaundray <kevtheappdev@gmail.com>
2022-12-05 14:31:15 +0000

    Update turbo_verifier.rs

--- src/contract/turbo_verifier.rs
+++ src/contract/turbo_verifier.rs
@@ -27,8 +27,8 @@ fn index(captures: &Captures) -> usize {
 }
 
 const TURBOVERIFIER: &str = r#"
-// SPDX-License-Identifier: GPL-2.0-only
-// Copyright 2020 Spilsbury Holdings Ltd
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2022 Aztec
 
 pragma solidity >=0.6.0 <0.8.0;
 pragma experimental ABIEncoderV2;
@@ -36,19 +36,6 @@ pragma experimental ABIEncoderV2;
 /**
  * @title Turbo Plonk proof verification contract
  * @dev Top level Plonk proof verification contract, which allows Plonk proof to be verified
- *
- * Copyright 2020 Spilsbury Holdings Ltd
- *
- * Licensed under the GNU General Public License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 contract TurboVerifier {
     using Bn254Crypto for Types.G1Point;
