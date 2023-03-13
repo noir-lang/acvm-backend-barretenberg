ff693be68f35230aad1171c9d90255d9ddb9e247
Blaine Bublitz <blaine@aztecprotocol.com>
2023-03-13 16:08:24 -0700

    fix!: Copy wasm into project during build instead of committing it

---
+++ barretenberg_wasm/build.rs
@@ -0,0 +1,31 @@
+use std::{env, fs};
+
+// Useful for printing debugging messages during the build
+// macro_rules! p {
+//     ($($tokens: tt)*) => {
+//         println!("cargo:warning={}", format!($($tokens)*))
+//     }
+// }
+
+fn copy_wasm(wasm_path: &String) -> Result<(), String> {
+    match fs::copy(wasm_path, "src/barretenberg.wasm") {
+        Ok(_) => Ok(()),
+        Err(err) => {
+            println!("{err:?}");
+            Err(format!("Failed to copy {wasm_path} into project. Does it exist?").into())
+        }
+    }
+}
+
+fn main() -> Result<(), String> {
+    match env::var("BARRETENBERG_WASM") {
+        Ok(wasm_path) => copy_wasm(&wasm_path),
+        Err(_) => {
+            if let Ok(bindir) = pkg_config::get_variable("barretenberg", "bindir") {
+                copy_wasm(&format!("{bindir}/barretenberg.wasm"))
+            } else {
+                Err("Unable to locate barretenberg.wasm - Please set the BARRETENBERG_WASM env var to the full path".into())
+            }
+        }
+    }
+}
