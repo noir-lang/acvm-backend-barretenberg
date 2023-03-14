1871f5ba708db7b38114ffc7a4f16dc613ac595b
Blaine Bublitz <blaine@aztecprotocol.com>
2023-03-14 14:03:13 -0700

    Use master branch

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
     };
 
     barretenberg = {
-      url = "github:AztecProtocol/barretenberg/phated/nix-plus-fixes";
+      url = "github:AztecProtocol/barretenberg";
       inputs = {
         nixpkgs.follows = "nixpkgs";
         flake-utils.follows = "flake-utils";
