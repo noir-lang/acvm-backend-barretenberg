cc6f1c785886f0f75d38540a565ce4ce13b562ac
Blaine Bublitz <blaine@aztecprotocol.com>
2023-03-15 16:01:24 -0700

    Switch to rust-embed to interpolate the nix env var

--- barretenberg_wasm/build.rs
+++ barretenberg_wasm/build.rs
@@ -1,4 +1,4 @@
-use std::{env, fs};
+use std::env;
 
 // Useful for printing debugging messages during the build
 // macro_rules! p {
@@ -7,24 +7,16 @@ use std::{env, fs};
 //     }
 // }
 
-fn copy_wasm(wasm_path: &String) -> Result<(), String> {
-    match fs::copy(wasm_path, "src/barretenberg.wasm") {
-        Ok(_) => Ok(()),
-        Err(err) => {
-            println!("{err:?}");
-            Err(format!(
-                "Failed to copy {wasm_path} into project. Does it exist?"
-            ))
-        }
-    }
-}
-
 fn main() -> Result<(), String> {
-    match env::var("BARRETENBERG_WASM") {
-        Ok(wasm_path) => copy_wasm(&wasm_path),
+    match env::var("BARRETENBERG_BIN_DIR") {
+        Ok(bindir) => {
+            println!("cargo:rustc-env=BARRETENBERG_BIN_DIR={bindir}");
+            Ok(())
+        }
         Err(_) => {
             if let Ok(bindir) = pkg_config::get_variable("barretenberg", "bindir") {
-                copy_wasm(&format!("{bindir}/barretenberg.wasm"))
+                println!("cargo:rustc-env=BARRETENBERG_BIN_DIR={bindir}");
+                Ok(())
             } else {
                 Err("Unable to locate barretenberg.wasm - Please set the BARRETENBERG_WASM env var to the full path".into())
             }
