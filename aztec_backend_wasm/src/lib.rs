b54441ece9955227f63f609c1048ae191a0d9631
Joss <joss@aztecprotocol.com>
2023-03-23 14:38:01 +0000

    chore(wasm) split js transforms into another file

--- aztec_backend_wasm/src/lib.rs
+++ aztec_backend_wasm/src/lib.rs
@@ -1,76 +1,18 @@
 use barretenberg_wasm;
 use common::acvm::{
-    acir::circuit::{opcodes::OracleData, Circuit, Opcode},
+    acir::circuit::{opcodes::OracleData, Opcode},
     acir::native_types::Witness,
     pwg::block::Blocks,
-    FieldElement, PartialWitnessGenerator,
+    PartialWitnessGenerator,
 };
 use js_sys::JsString;
-use std::collections::BTreeMap;
+use js_transforms::{
+    field_element_to_js_string, format_js_err, js_map_to_witness_map, js_value_to_field_element,
+    read_circuit, witness_map_to_js_map, JsErrorString,
+};
 use wasm_bindgen::prelude::*;
 
-type JsErrorString = JsString;
-type WitnessMap = BTreeMap<Witness, FieldElement>;
-
-fn js_value_to_field_element(js_value: JsValue) -> Result<FieldElement, JsErrorString> {
-    let hex_str = match js_value.as_string() {
-        Some(str) => str,
-        None => return Err("failed to parse field element from non-string".into()),
-    };
-    match FieldElement::from_hex(&hex_str) {
-        Some(field_element) => Ok(field_element),
-        None => Err(format!("Invalid hex string: '{}'", hex_str).into()),
-    }
-}
-
-fn js_map_to_witness_map(js_map: js_sys::Map) -> Result<WitnessMap, JsErrorString> {
-    let mut witness_assignments: BTreeMap<Witness, FieldElement> = BTreeMap::new();
-    for key_result in js_map.keys() {
-        let key = match key_result {
-            Ok(key) => key,
-            Err(_) => return Err("bad key".into()),
-        };
-        let idx;
-        unsafe {
-            idx = match key.as_f64() {
-                Some(value) => value.to_int_unchecked::<u32>(),
-                None => return Err("not a number".into()),
-            }
-        }
-        let field_element = js_value_to_field_element(js_map.get(&key))?;
-        witness_assignments.insert(Witness(idx), field_element);
-    }
-    Ok(witness_assignments)
-}
-
-fn field_element_to_js_string(field_element: &FieldElement) -> JsString {
-    format!("0x{}", field_element.to_hex()).into()
-}
-
-fn witness_map_to_js_map(witness_map: BTreeMap<Witness, FieldElement>) -> js_sys::Map {
-    let js_map = js_sys::Map::new();
-    for (witness, field_value) in witness_map.iter() {
-        let js_idx = js_sys::Number::from(witness.0);
-        let js_hex_str = field_element_to_js_string(field_value);
-        js_map.set(&js_idx, &js_hex_str);
-    }
-    js_map
-}
-
-fn read_circuit(circuit: js_sys::Uint8Array) -> Result<Circuit, JsErrorString> {
-    let circuit: Vec<u8> = circuit.to_vec();
-    match Circuit::read(&*circuit) {
-        Ok(circuit) => Ok(circuit),
-        Err(err) => Err(format!("Circuit read err: {}", err).into()),
-    }
-}
-
-fn format_js_err(err: JsValue) -> String {
-    match err.as_string() {
-        Some(str) => str,
-        None => "Unknown".to_owned(),
-    }
-}
+mod js_transforms;
 
 async fn resolve_oracle(
     oracle_resolver: &js_sys::Function,
