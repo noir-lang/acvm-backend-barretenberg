8289c5aebb8bedd968cf86594bcb76b27094b000
Tom French <tom@tomfren.ch>
2023-03-24 18:07:23 +0000

    chore!: update to ACVM 0.7.0

--- common/src/serializer.rs
+++ common/src/serializer.rs
@@ -341,8 +341,8 @@ pub fn serialize_circuit(circuit: &Circuit) -> ConstraintSystem {
             Opcode::Directive(_) => {
                 // Directives are only needed by the pwg
             }
-            Opcode::Block(_, _) => {
-                // TODO: implement serialization of blocks to match BB's interface
+            Opcode::Block(_) | Opcode::RAM(_) | Opcode::ROM(_) | Opcode::Oracle(_) => {
+                // TODO: implement serialization to match BB's interface
             }
         }
     }
@@ -350,7 +350,7 @@ pub fn serialize_circuit(circuit: &Circuit) -> ConstraintSystem {
     // Create constraint system
     ConstraintSystem {
         var_num: circuit.current_witness_index + 1, // number of witnesses is the witness index + 1;
-        public_inputs: circuit.public_inputs.indices(),
+        public_inputs: circuit.public_inputs().indices(),
         logic_constraints,
         range_constraints,
         sha256_constraints,
