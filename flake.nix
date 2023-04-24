e98a574c631fea1ee0d69beeb4edf793db910fbb
Blaine Bublitz <blaine.bublitz@gmail.com>
2023-04-24 15:23:52 +0100

    chore: bring project up to date with Nix patterns (#131)
    * chore: Bring project up to date with Nix patterns
    
    * cspell
    
    * chore: update readme to refresh CI
    
    * Rename workflow
    
    ---------
    
    Co-authored-by: Tom French <tom@tomfren.ch>

--- flake.nix
+++ flake.nix
@@ -2,25 +2,45 @@
   description = "Build the Barretenberg acvm backend";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
-
-    crane = {
-      url = "github:ipetkov/crane";
-      inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-22.11";
     };
 
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
 
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
+      # All of these inputs (a.k.a. dependencies) need to align with inputs we
+      # use so they use the `inputs.*.follows` syntax to reference our inputs
       inputs = {
         nixpkgs.follows = "nixpkgs";
         flake-utils.follows = "flake-utils";
       };
     };
 
+    crane = {
+      url = "github:ipetkov/crane";
+      # All of these inputs (a.k.a. dependencies) need to align with inputs we
+      # use so they use the `inputs.*.follows` syntax to reference our inputs
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+        flake-compat.follows = "flake-compat";
+        rust-overlay.follows = "rust-overlay";
+      };
+    };
+
     barretenberg = {
       url = "github:AztecProtocol/barretenberg";
+      # All of these inputs (a.k.a. dependencies) need to align with inputs we
+      # use so they use the `inputs.*.follows` syntax to reference our inputs
       inputs = {
         nixpkgs.follows = "nixpkgs";
         flake-utils.follows = "flake-utils";
@@ -40,13 +60,24 @@
         ];
       };
 
-      rustToolchain = pkgs.rust-bin.stable."1.66.0".default;
+      rustToolchain = pkgs.rust-bin.stable."1.66.0".default.override {
+        # We include rust-src to ensure rust-analyzer works.
+        # See https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570/4
+        extensions = [ "rust-src" ];
+      };
 
       craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
 
       environment = {
         # rust-bindgen needs to know the location of libclang
         LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+
+        # Barretenberg fails if tests are run on multiple threads, so we set the test thread
+        # count to 1 throughout the entire project
+        #
+        # Note: Setting this allows for consistent behavior across build and shells, but is mostly
+        # hidden from the developer - i.e. when they see the command being run via `nix flake check`
+        RUST_TEST_THREADS = "1";
 
         # We set the environment variable because barretenberg must be compiled in a special way for wasm
         BARRETENBERG_BIN_DIR = "${pkgs.barretenberg-wasm}/bin";
@@ -62,25 +93,32 @@
       sourceFilter = path: type:
         (solidityFilter path type) || (craneLib.filterCargoSources path type);
 
-      commonArgs = {
+      # As per https://discourse.nixos.org/t/gcc11stdenv-and-clang/17734/7 since it seems that aarch64-linux uses
+      # gcc9 instead of gcc11 for the C++ stdlib, while all other targets we support provide the correct libstdc++
+      stdenv =
+        if (pkgs.stdenv.targetPlatform.isGnu && pkgs.stdenv.targetPlatform.isAarch64) then
+          pkgs.overrideCC pkgs.llvmPackages.stdenv (pkgs.llvmPackages.clang.override { gccForLibs = pkgs.gcc11.cc; })
+        else
+          pkgs.llvmPackages.stdenv;
+
+      # Combine the environment and other configuration needed for crane to build our Rust packages
+      commonArgs = environment // {
         # TODO: Rename repository
         pname = "acvm-backend-barretenberg";
-        # TODO: Add release-please
+        # x-release-please-start-version
         version = "0.0.0";
+        # x-release-please-end
 
-        # As per https://discourse.nixos.org/t/gcc11stdenv-and-clang/17734/7 since it seems that aarch64-linux uses
-        # gcc9 instead of gcc11 for the C++ stdlib, while all other targets we support provide the correct libstdc++
-        stdenv = with pkgs;
-          if (stdenv.targetPlatform.isGnu && stdenv.targetPlatform.isAarch64) then
-            overrideCC llvmPackages.stdenv (llvmPackages.clang.override { gccForLibs = gcc11.cc; })
-          else
-            llvmPackages.stdenv;
+        # Use our custom stdenv to build and test our Rust project
+        inherit stdenv;
 
         src = pkgs.lib.cleanSourceWith {
           src = craneLib.path ./.;
           filter = sourceFilter;
         };
 
+        # Running checks don't do much more than compiling itself and increase
+        # the build time by a lot, so we disable them throughout all our flakes
         doCheck = false;
 
         nativeBuildInputs = [
@@ -98,7 +136,7 @@
           pkgs.libiconv
           pkgs.darwin.apple_sdk.frameworks.Security
         ];
-      } // environment;
+      };
 
       # Build *just* the cargo dependencies, so we can reuse all of that work between runs
       cargoArtifacts = craneLib.buildDepsOnly commonArgs;
@@ -112,40 +150,39 @@
         cargo-clippy = craneLib.cargoClippy (commonArgs // {
           inherit cargoArtifacts;
 
-          cargoClippyExtraArgs = "--all-targets --workspace -- -D warnings";
-
-          doCheck = true;
+          cargoClippyExtraArgs = "--all-targets -- -D warnings";
         });
 
         cargo-test = craneLib.cargoTest (commonArgs // {
           inherit cargoArtifacts;
 
-          cargoTestExtraArgs = "--workspace -- --test-threads=1";
+          cargoTestExtraArgs = "--workspace";
 
+          # It's unclear why doCheck needs to be enabled for tests to run but not clippy
           doCheck = true;
         });
       };
 
       packages.default = acvm-backend-barretenberg;
 
-      # llvmPackages should be aligned to selection from libbarretenberg
-      # better if we get rid of llvm targets and override them from input
-      devShells.default = pkgs.mkShell.override { stdenv = pkgs.llvmPackages.stdenv; } ({
-        inputsFrom = builtins.attrValues self.checks;
+      # Setup the environment to match the stdenv from `nix build` & `nix flake check`, and
+      # combine it with the environment settings, the inputs from our checks derivations,
+      # and extra tooling via `nativeBuildInputs`
+      devShells.default = pkgs.mkShell.override { inherit stdenv; } (environment // {
+        inputsFrom = builtins.attrValues checks;
 
-        buildInputs = packages.default.buildInputs;
-        nativeBuildInputs = with pkgs; packages.default.nativeBuildInputs ++ [
+        nativeBuildInputs = with pkgs; [
           which
           starship
           git
-          cargo
-          rustc
+          nil
+          nixpkgs-fmt
+          llvmPackages.lldb # This ensures the right lldb is in the environment for running rust-lldb
         ];
 
         shellHook = ''
           eval "$(starship init bash)"
         '';
-
-      } // environment);
+      });
     });
 }
