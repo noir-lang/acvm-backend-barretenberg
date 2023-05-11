1883f198df91aee743935467575378e4a8e9176d
Koby Hall <102518238+kobyhallx@users.noreply.github.com>
2023-05-11 17:43:02 +0200

    chore: Release 0.1.1

--- flake.nix
+++ flake.nix
@@ -114,7 +114,7 @@
       commonArgs = {
         pname = "acvm-backend-barretenberg";
         # x-release-please-start-version
-        version = "0.1.0";
+        version = "0.1.1";
         # x-release-please-end
 
         src = pkgs.lib.cleanSourceWith {
