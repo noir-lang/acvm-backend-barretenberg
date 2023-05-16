04c9d0f6b5e0f3625fd7d1e2bb5bacb020ae279d
Blaine Bublitz <blaine.bublitz@gmail.com>
2023-05-16 06:03:15 +0100

    feat!: Implement CommonReferenceString trait (#139)
    * chore!: Remove filesystem access and utilize async fetch with range requests
    
    Co-authored-by: Tom French <tom@tomfren.ch>

--- src/acvm_interop/smart_contract.rs
+++ src/acvm_interop/smart_contract.rs
@@ -1,7 +1,6 @@
 use acvm::SmartContract;
 
-use crate::crs::G2;
-use crate::{BackendError, Barretenberg};
+use crate::{crs::CRS, BackendError, Barretenberg};
 
 /// Embed the Solidity verifier file
 const ULTRA_VERIFIER_CONTRACT: &str = include_str!("contract.sol");
@@ -10,10 +9,14 @@ const ULTRA_VERIFIER_CONTRACT: &str = include_str!("contract.sol");
 impl SmartContract for Barretenberg {
     type Error = BackendError;
 
-    fn eth_contract_from_vk(&self, verification_key: &[u8]) -> Result<String, Self::Error> {
+    fn eth_contract_from_vk(
+        &self,
+        common_reference_string: &[u8],
+        verification_key: &[u8],
+    ) -> Result<String, Self::Error> {
         use std::slice;
 
-        let g2 = G2::new();
+        let CRS { g2_data, .. } = common_reference_string.try_into()?;
 
         let mut contract_ptr: *mut u8 = std::ptr::null_mut();
         let p_contract_ptr = &mut contract_ptr as *mut *mut u8;
@@ -22,7 +25,7 @@ impl SmartContract for Barretenberg {
         let contract_size;
         unsafe {
             contract_size = barretenberg_sys::composer::get_solidity_verifier(
-                &g2.data,
+                &g2_data,
                 &verification_key,
                 p_contract_ptr,
             );
@@ -40,10 +43,14 @@ impl SmartContract for Barretenberg {
 impl SmartContract for Barretenberg {
     type Error = BackendError;
 
-    fn eth_contract_from_vk(&self, verification_key: &[u8]) -> Result<String, Self::Error> {
-        let g2 = G2::new();
+    fn eth_contract_from_vk(
+        &self,
+        common_reference_string: &[u8],
+        verification_key: &[u8],
+    ) -> Result<String, Self::Error> {
+        let CRS { g2_data, .. } = common_reference_string.try_into()?;
 
-        let g2_ptr = self.allocate(&g2.data)?;
+        let g2_ptr = self.allocate(&g2_data)?;
         let vk_ptr = self.allocate(verification_key)?;
 
         // The smart contract string is not actually written to this pointer.
@@ -74,6 +81,7 @@ fn test_smart_contract() -> Result<(), BackendError> {
     use crate::composer::Composer;
     use crate::Barretenberg;
     use acvm::FieldElement;
+    use tokio::runtime::Builder;
 
     let constraint = Constraint {
         a: 1,
@@ -92,11 +100,18 @@ fn test_smart_contract() -> Result<(), BackendError> {
         .constraints(vec![constraint]);
 
     let bb = Barretenberg::new();
+    let crs = Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(bb.get_crs(&constraint_system))?;
 
     let proving_key = bb.compute_proving_key(&constraint_system)?;
-    let verification_key = bb.compute_verification_key(&constraint_system, &proving_key)?;
+    let verification_key = bb.compute_verification_key(&crs, &proving_key)?;
 
-    let contract = bb.eth_contract_from_vk(&verification_key)?;
+    let common_reference_string: Vec<u8> = crs.try_into()?;
+
+    let contract = bb.eth_contract_from_vk(&common_reference_string, &verification_key)?;
 
     assert!(contract.contains("contract BaseUltraVerifier"));
     assert!(contract.contains("contract UltraVerifier"));
