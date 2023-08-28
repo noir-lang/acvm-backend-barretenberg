434c2173f967e194b85f9686e9f8d9eeecb2c975
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2023-08-28 20:57:56 +0100

    set env variables once all checks have passed

--- build.rs
+++ build.rs
@@ -10,26 +10,23 @@ use build_target::{Arch, Os};
 fn main() -> Result<(), String> {
     // We need to inject which OS we're building for so that we can download the correct barretenberg binary.
     let os = match build_target::target_os().unwrap() {
-        os @ (Os::Linux | Os::MacOs) => {
-            println!("cargo:rustc-env=TARGET_OS={os}");
-            os
-        }
+        os @ (Os::Linux | Os::MacOs) => os,
         Os::Windows => todo!("Windows is not currently supported"),
         os_name => panic!("Unsupported OS {}", os_name),
     };
 
     let arch = match build_target::target_arch().unwrap() {
-        arch @ (Arch::X86_64 | Arch::AARCH64) => {
-            println!("cargo:rustc-env=TARGET_ARCH={arch}");
-            arch
-        }
+        arch @ (Arch::X86_64 | Arch::AARCH64) => arch,
         arch_name => panic!("Unsupported Architecture {}", arch_name),
     };
 
     // Arm builds of linux are not supported
-    if let (Os::Linux, Arch::AARCH64) = (os, arch) {
+    if let (Os::Linux, Arch::AARCH64) = (&os, &arch) {
         panic!("ARM64 builds of linux are not supported")
     };
+
+    println!("cargo:rustc-env=TARGET_OS={os}");
+    println!("cargo:rustc-env=TARGET_ARCH={arch}");
 
     Ok(())
 }
