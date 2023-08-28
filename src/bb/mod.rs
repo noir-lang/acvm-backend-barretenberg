fae8a7375e59c0c60f64e1fa1aa90d02063232b6
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2023-08-28 20:08:27 +0100

    now using version 0.4.4

--- src/bb/mod.rs
+++ src/bb/mod.rs
@@ -22,7 +22,7 @@ pub(crate) struct CliShimError(String);
 
 const USERNAME: &str = "AztecProtocol";
 const REPO: &str = "barretenberg";
-const VERSION: &str = "0.4.2";
+const VERSION: &str = "0.4.4";
 const TAG: &str = formatcp!("barretenberg-v{}", VERSION);
 const DEST_FOLDER: &str = ".nargo/backends/acvm-backend-barretenberg";
 const BINARY_NAME: &str = "backend_binary";
