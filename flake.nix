807cc3962c706c5a907dbc00b0ac402e7af366d6
Tom French <tom@tomfren.ch>
2023-08-29 17:57:04 +0100

    chore: remove wasm filter

--- flake.nix
+++ flake.nix
@@ -81,7 +81,6 @@
         };
       };
 
-      wasmFilter = path: _type: builtins.match ".*wasm$" path != null;
       # We use `include_str!` macro to embed the solidity verifier template so we need to create a special
       # source filter to include .sol files in addition to usual rust/cargo source files.
       solidityFilter = path: _type: builtins.match ".*sol$" path != null;
@@ -89,7 +88,7 @@
       bytecodeFilter = path: _type: builtins.match ".*bytecode$" path != null;
       witnessFilter = path: _type: builtins.match ".*tr$" path != null;
       sourceFilter = path: type:
-        (solidityFilter path type) || (bytecodeFilter path type)|| (wasmFilter path type) || (witnessFilter path type) || (craneLib.filterCargoSources path type);
+        (solidityFilter path type) || (bytecodeFilter path type)|| (witnessFilter path type) || (craneLib.filterCargoSources path type);
 
       # As per https://discourse.nixos.org/t/gcc11stdenv-and-clang/17734/7 since it seems that aarch64-linux uses
       # gcc9 instead of gcc11 for the C++ stdlib, while all other targets we support provide the correct libstdc++
